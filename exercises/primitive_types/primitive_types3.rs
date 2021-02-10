b4e2c4a9a3678d61dee5ecc5cfc26613c28be0e4
Sandro Pratesi <sandro.pratesi@gmx.com>
2021-02-10 14:41:03 +0100

    wip

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,10 +2,9 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` for hints!
 
-// I AM NOT DONE
 
 fn main() {
-    let a = (-1..=1266).collect::<Vec<i32>>().try_into();
+    let a = [6i32; 500];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array! {:?}", a);
